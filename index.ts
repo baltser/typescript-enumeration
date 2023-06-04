18726c209065e570df76be757a43b7fa166ced0e
baltser <110670480+baltser@users.noreply.github.com>
2023-06-04 20:49:56 +0400

    Enums

--- index.ts
+++ index.ts
@@ -21,3 +21,13 @@
  console.log(`70F is ${convertTemperature(70, 'FtoC')}C`)
  console.log(`21C is ${convertTemperature(21, 'CtoF')}C`)
  console.log(`35C is ${convertTemperature(35, 'ABCD')}C`)
+
+ enum Direction {
+      FtoC,
+      CtoF
+ }
+ function convertTemperatureEnum(temp: number, fromTo: Direction): number {
+  return (Direction.FtoC === fromTo) ? (temp - 32) * 5.0 / 9.0 : temp * 0.9 / 5.0 + 32;
+}
+console.log(`70F is ${convertTemperatureEnum(70, Direction.FtoC)}C`)
+console.log(`21C is ${convertTemperatureEnum(21, Direction.CtoF)}C`)
